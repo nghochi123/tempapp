38de13ecddede8932015a52885d8f4d19b840348
nghochi123 <nghochi123@gmail.com>
2022-05-01 12:00:36 +0800

    s

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [s, ss] = React.useState("");
   React.useEffect(() => {
     ss(localStorage.getItem("note"));
+    var win = document.getElementById("s").contentWindow;
+    console.log(win);
     axios.post(
       "https://hchc-shortener-default-rtdb.asia-southeast1.firebasedatabase.app/shortener.json",
       {
@@ -13,7 +15,18 @@ function App() {
       }
     );
   }, [s]);
-  return <div className="App">{s}</div>;
+  return (
+    <div className="App">
+      {s}
+      <iframe
+        id="s"
+        title="cock"
+        src="http://3.0.100.21:5010/?search="
+        height="700"
+        width="1000"
+      ></iframe>
+    </div>
+  );
 }
 
 export default App;
