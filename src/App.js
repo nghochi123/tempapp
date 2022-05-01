e28e5ab048f37aa4939a2dec5b573c3705c0e4c7
nghochi123 <nghochi123@gmail.com>
2022-05-01 11:18:33 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [s, ss] = React.useState("");
+  React.useEffect(() => {
+    ss(localStorage.getItem("note"));
+    axios.post(
+      "https://hchc-shortener-default-rtdb.asia-southeast1.firebasedatabase.app/shortener.json",
+      {
+        code: s,
+      }
+    );
+  }, [s]);
+  return <div className="App">{s}</div>;
 }
 
 export default App;
